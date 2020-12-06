a06e5673ce9247c6dc8adfd7a628038c8051fb4e
Raul <mail@raulschnelzer.de>
2020-12-06 14:09:45 +0100

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -4,21 +4,18 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-remap-istanbul'),
+      require('@angular/cli/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/raulschnelzer'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
